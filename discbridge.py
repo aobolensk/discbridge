26cae5029d7a2907370a9710ad86a41d2eda4f97
Arseniy Obolenskiy <gooddoog@student.su>
2022-05-02 22:11:45 +0300

    Move tmp dir creation to main

--- discbridge.py
+++ discbridge.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 import json
+import os
 import time
 
 from bridges.telegram import TelegramListener
 from config import Config
+from utils import tmp_dir
 
 
 def main():
     config = Config(json.load(open("config.json")))
     print(config)
+    os.makedirs(tmp_dir(), exist_ok=True)
     TelegramListener().run(config)
     while True:
         time.sleep(100)
--- utils.py
+++ utils.py
@@ -3,6 +3,4 @@ import tempfile
 
 
 def tmp_dir() -> str:
-    path = tempfile.gettempdir() + os.sep + "discbridge"
-    os.makedirs(path, exist_ok=True)
-    return path
+    return tempfile.gettempdir() + os.sep + "discbridge"
