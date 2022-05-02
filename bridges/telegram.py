7598402f1ba352ad8ee5a715b9de1b6454244dee
Arseniy Obolenskiy <gooddoog@student.su>
2022-05-02 22:10:12 +0300

    Add support for video messages

--- bridges/telegram.py
+++ bridges/telegram.py
@@ -46,7 +46,6 @@ class TelegramListener(Listener):
     def _on_voice(self, update: Update, context: CallbackContext) -> None:
         if update.message.chat.id not in self._config.telegram.chat_ids:
             return
-        print("_on_voice")
         r = requests.get(f"https://api.telegram.org/bot{self._config.telegram.token}/getFile?file_id={update.message.voice.file_id}")
         tgs_file_path = r.json()["result"]["file_path"]
         r = requests.get(f"https://api.telegram.org/file/bot{self._config.telegram.token}/{tgs_file_path}", stream=True)
@@ -61,6 +60,23 @@ class TelegramListener(Listener):
         webhook.add_file(file=open(f"{tmp_dir()}/file.ogg", "rb").read(), filename=f"{tmp_dir()}/file.ogg")
         webhook.execute()
 
+    def _on_videonote(self, update: Update, context: CallbackContext) -> None:
+        if update.message.chat.id not in self._config.telegram.chat_ids:
+            return
+        r = requests.get(f"https://api.telegram.org/bot{self._config.telegram.token}/getFile?file_id={update.message.video_note.file_id}")
+        tgs_file_path = r.json()["result"]["file_path"]
+        r = requests.get(f"https://api.telegram.org/file/bot{self._config.telegram.token}/{tgs_file_path}", stream=True)
+        if r.status_code == 200:
+            with open(f"{tmp_dir()}/file.mp4", "wb") as f:
+                r.raw.decode_content = True
+                shutil.copyfileobj(r.raw, f)
+        text = f"{update.message.from_user.username}: {update.message.text or ''}"
+        webhook = DiscordWebhook(
+            url=self._config.discord.webhook_link, rate_limit_retry=True,
+            content=text)
+        webhook.add_file(file=open(f"{tmp_dir()}/file.mp4", "rb").read(), filename=f"{tmp_dir()}/file.mp4")
+        webhook.execute()
+
     def start(self, config: Config):
         print("start")
         self._config = config
@@ -69,6 +85,7 @@ class TelegramListener(Listener):
         dispatcher.add_handler(MessageHandler(Filters.text, self._on_message))
         dispatcher.add_handler(MessageHandler(Filters.sticker, self._on_sticker))
         dispatcher.add_handler(MessageHandler(Filters.voice, self._on_voice))
+        dispatcher.add_handler(MessageHandler(Filters.video_note, self._on_videonote))
 
         updater.start_polling()
         while True:
