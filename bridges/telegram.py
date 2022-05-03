d1fcc4a5d0ae023ca6d43f58a2025538623f2e7a
Arseniy Obolenskiy <gooddoog@student.su>
2022-05-03 18:44:45 +0300

    Abstrct out downloading files

--- bridges/telegram.py
+++ bridges/telegram.py
@@ -14,6 +14,15 @@ from telegram.ext import CallbackContext, Filters, MessageHandler, Updater
 
 
 class TelegramListener(Listener):
+    def _download_file(self, file_id: str, output_file: str):
+        r = requests.get(f"https://api.telegram.org/bot{self._config.telegram.token}/getFile?file_id={file_id}")
+        target_file_path = r.json()["result"]["file_path"]
+        r = requests.get(f"https://api.telegram.org/file/bot{self._config.telegram.token}/{target_file_path}", stream=True)
+        if r.status_code == 200:
+            with open(output_file, "wb") as f:
+                r.raw.decode_content = True
+                shutil.copyfileobj(r.raw, f)
+
     def _on_message(self, update: Update, context: CallbackContext) -> None:
         if update.message.chat.id not in self._config.telegram.chat_ids:
             print(f"{datetime.datetime.now()}: filtered id: {update.message.chat.id}")
@@ -28,53 +37,39 @@ class TelegramListener(Listener):
     def _on_sticker(self, update: Update, context: CallbackContext) -> None:
         if update.message.chat.id not in self._config.telegram.chat_ids:
             return
-        r = requests.get(f"https://api.telegram.org/bot{self._config.telegram.token}/getFile?file_id={update.message.sticker.file_id}")
-        tgs_file_path = r.json()["result"]["file_path"]
-        r = requests.get(f"https://api.telegram.org/file/bot{self._config.telegram.token}/{tgs_file_path}", stream=True)
-        if r.status_code == 200:
-            with open(f"{tmp_dir()}/file.tgs", "wb") as f:
-                r.raw.decode_content = True
-                shutil.copyfileobj(r.raw, f)
-        subprocess.call(f"lottie_convert.py {tmp_dir()}/file.tgs {tmp_dir()}/file.gif", shell=True)
+        tgs_file = tmp_dir() + "/file.tgs"
+        gif_file = tmp_dir() + "/file.gif"
+        self._download_file(update.message.sticker.file_id, tgs_file)
+        subprocess.call(f"lottie_convert.py {tgs_file} {gif_file}", shell=True)
         text = f"{update.message.from_user.username}: {update.message.text or ''}"
         webhook = DiscordWebhook(
             url=self._config.discord.webhook_link, rate_limit_retry=True,
             content=text)
-        webhook.add_file(file=open(f"{tmp_dir()}/file.gif", "rb").read(), filename=f"{tmp_dir()}/file.gif")
+        webhook.add_file(file=open(gif_file, "rb").read(), filename=gif_file)
         webhook.execute()
 
     def _on_voice(self, update: Update, context: CallbackContext) -> None:
         if update.message.chat.id not in self._config.telegram.chat_ids:
             return
-        r = requests.get(f"https://api.telegram.org/bot{self._config.telegram.token}/getFile?file_id={update.message.voice.file_id}")
-        tgs_file_path = r.json()["result"]["file_path"]
-        r = requests.get(f"https://api.telegram.org/file/bot{self._config.telegram.token}/{tgs_file_path}", stream=True)
-        if r.status_code == 200:
-            with open(f"{tmp_dir()}/file.ogg", "wb") as f:
-                r.raw.decode_content = True
-                shutil.copyfileobj(r.raw, f)
+        ogg_file = tmp_dir() + "/file.ogg"
+        self._download_file(update.message.voice.file_id, ogg_file)
         text = f"{update.message.from_user.username}: {update.message.text or ''}"
         webhook = DiscordWebhook(
             url=self._config.discord.webhook_link, rate_limit_retry=True,
             content=text)
-        webhook.add_file(file=open(f"{tmp_dir()}/file.ogg", "rb").read(), filename=f"{tmp_dir()}/file.ogg")
+        webhook.add_file(file=open(ogg_file, "rb").read(), filename=ogg_file)
         webhook.execute()
 
     def _on_videonote(self, update: Update, context: CallbackContext) -> None:
         if update.message.chat.id not in self._config.telegram.chat_ids:
             return
-        r = requests.get(f"https://api.telegram.org/bot{self._config.telegram.token}/getFile?file_id={update.message.video_note.file_id}")
-        tgs_file_path = r.json()["result"]["file_path"]
-        r = requests.get(f"https://api.telegram.org/file/bot{self._config.telegram.token}/{tgs_file_path}", stream=True)
-        if r.status_code == 200:
-            with open(f"{tmp_dir()}/file.mp4", "wb") as f:
-                r.raw.decode_content = True
-                shutil.copyfileobj(r.raw, f)
+        mp4_file = tmp_dir() + "/file.mp4"
+        self._download_file(update.message.video_note.file_id, mp4_file)
         text = f"{update.message.from_user.username}: {update.message.text or ''}"
         webhook = DiscordWebhook(
             url=self._config.discord.webhook_link, rate_limit_retry=True,
             content=text)
-        webhook.add_file(file=open(f"{tmp_dir()}/file.mp4", "rb").read(), filename=f"{tmp_dir()}/file.mp4")
+        webhook.add_file(file=open(mp4_file, "rb").read(), filename=mp4_file)
         webhook.execute()
 
     def start(self, config: Config):
