34261b36d3d8e42b215901a9a59a507e71fe5c30
Arseniy Obolenskiy <gooddoog@student.su>
2022-05-08 13:19:32 +0300

    Prettify status update messages

--- input/telegram.py
+++ input/telegram.py
@@ -81,41 +81,41 @@ class TelegramListener(Listener):
         text = self._format_backend_header(update.message) + "Channel message: "
         msg = update.message
         if msg.group_chat_created:
-            text += "group_chat_created: " + msg.group_chat_created
+            text += "Group chat created: " + msg.group_chat_created
         if msg.supergroup_chat_created:
-            text += "supergroup_chat_created: " + msg.supergroup_chat_created
+            text += "Supergroup chat created: " + msg.supergroup_chat_created
         if msg.channel_chat_created:
-            text += "channel_chat_created: " + msg.channel_chat_created
+            text += "Channel chat created: " + msg.channel_chat_created
         if msg.connected_website:
-            text += "connected_website: " + msg.connected_website
+            text += "Connected website: " + msg.connected_website
         if msg.delete_chat_photo:
-            text += "delete_chat_photo: " + msg.delete_chat_photo
+            text += "Delete chat photo: " + msg.delete_chat_photo
         if msg.left_chat_member:
-            text += "left_chat_member: " + msg.left_chat_member
+            text += "Left chat member: " + msg.left_chat_member
         if msg.migrate_to_chat_id:
-            text += "migrate_to_chat_id: " + msg.migrate_to_chat_id
+            text += "Migrate to chat id: " + msg.migrate_to_chat_id
         if msg.migrate_from_chat_id:
-            text += "migrate_from_chat_id: " + msg.migrate_from_chat_id
+            text += "Migrate from chat id: " + msg.migrate_from_chat_id
         if msg.new_chat_members:
-            text += "new_chat_members: " + msg.new_chat_members
+            text += "New chat members: " + msg.new_chat_members
         if msg.new_chat_photo:
-            text += "new_chat_photo: " + msg.new_chat_photo
+            text += "New chat photo: " + msg.new_chat_photo
         if msg.new_chat_title:
-            text += "new_chat_title: " + msg.new_chat_title
+            text += "New chat title: " + msg.new_chat_title
         if msg.message_auto_delete_timer_changed:
-            text += "message_auto_delete_timer_changed: " + msg.message_auto_delete_timer_changed
+            text += "Message auto delete timer changed: " + msg.message_auto_delete_timer_changed
         if msg.pinned_message:
-            text += "pinned_message: " + msg.pinned_message
+            text += "Pinned message: " + msg.pinned_message
         if msg.proximity_alert_triggered:
-            text += "proximity_alert_triggered: " + msg.proximity_alert_triggered
+            text += "Proximity alert triggered: " + msg.proximity_alert_triggered
         if msg.voice_chat_scheduled:
-            text += "voice_chat_scheduled: " + msg.voice_chat_scheduled
+            text += "Voice chat scheduled: " + msg.voice_chat_scheduled
         if msg.voice_chat_started:
-            text += "voice_chat_started: " + msg.voice_chat_started
+            text += "Voice chat started: " + msg.voice_chat_started
         if msg.voice_chat_ended:
-            text += "voice_chat_ended: " + msg.voice_chat_ended
+            text += "Voice chat ended: " + msg.voice_chat_ended
         if msg.voice_chat_participants_invited:
-            text += "voice_chat_participants_invited: " + msg.voice_chat_participants_invited
+            text += "Voice chat participants invited: " + msg.voice_chat_participants_invited
         self._core.send_message(text)
 
     def start(self, core, config: Config):
