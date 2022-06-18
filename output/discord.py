290daa0f3979fc74b2c06baafd76840941d222f7
Arseniy Obolenskiy <gooddoog@student.su>
2022-06-18 18:32:12 +0300

    Support proxy on output/discord

--- output/discord.py
+++ output/discord.py
@@ -3,6 +3,7 @@ from typing import List
 from config import Config
 from discord_webhook import DiscordWebhook
 from logger import log
+from utils import proxy
 
 from output.abc import Handler
 
@@ -11,11 +12,15 @@ class DiscordHandler(Handler):
     def init(self, core, config: Config) -> None:
         self._core = core
         self._config = config
+        self._proxy = {
+            "http": proxy.http(),
+            "https": proxy.https(),
+        }
 
     def send_message(self, text: str, files: List[str] = []):
         webhook = DiscordWebhook(
             url=self._config.output.discord.webhook_link, rate_limit_retry=True,
-            content=text)
+            content=text, proxies=self._proxy)
         for file in files:
             webhook.add_file(file=open(file, "rb").read(), filename=file)
         resp = webhook.execute()
@@ -31,7 +36,7 @@ class DiscordHandler(Handler):
     def _retry_without_files(self, text: str):
         webhook = DiscordWebhook(
             url=self._config.output.discord.webhook_link, rate_limit_retry=True,
-            content=text)
+            content=text, proxies=self._proxy)
         resp = webhook.execute()
         if resp.status_code in (200, 204):
             log.message(self.__class__.__name__, text)
