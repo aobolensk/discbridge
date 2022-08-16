1d563126585c2ab62ae96a1908f91a7d8119ca1e
Arseniy Obolenskiy <gooddoog@student.su>
2022-08-16 12:13:10 +0300

    Add support for sending files in matrix output backend

--- output/matrix.py
+++ output/matrix.py
@@ -1,11 +1,16 @@
 import asyncio
 import json
+import os
 import threading
 from typing import List
 
+import aiofiles
+import aiofiles.os
+import magic
 from config import Config
 from logger import log
 from nio import AsyncClient
+from PIL import Image
 from utils import proxy
 
 from output.abc import Handler
@@ -36,19 +41,64 @@ class MatrixHandler(Handler):
         )
         await self._client.sync(full_state=True)
 
-    async def _send_message_impl(self, text):
+    async def _send_message_impl(self, text, files):
         await self._client.room_send(
             room_id=self._config.output[self.get_instance_name()].room_id,
             message_type="m.room.message",
             content={"msgtype": "m.text", "body": text},
             ignore_unverified_devices=True,
         )
+        for file in files:
+            mime_type = magic.from_file(file, mime=True)
+            file_stat = await aiofiles.os.stat(file)
+            async with aiofiles.open(file, "r+b") as f:
+                resp, _ = await self._client.upload(
+                    f,
+                    content_type=mime_type,
+                    filename=os.path.basename(file),
+                    filesize=file_stat.st_size)
+            if mime_type.startswith("image/"):
+                im = Image.open(file)
+                width, height = im.size
+                content = {
+                    "body": os.path.basename(file),
+                    "info": {
+                        "size": file_stat.st_size,
+                        "mimetype": mime_type,
+                        "thumbnail_info": None,
+                        "w": width,
+                        "h": height,
+                        "thumbnail_url": None,
+                    },
+                    "msgtype": "m.image",
+                    "url": resp.content_uri,
+                }
+                await self._client.room_send(
+                    self._config.output[self.get_instance_name()].room_id,
+                    message_type="m.room.message",
+                    content=content
+                )
+            else:
+                content = {
+                    "body": os.path.basename(file),
+                    "info": {
+                        "size": file_stat.st_size,
+                        "mimetype": mime_type,
+                    },
+                    "msgtype": "m.file",
+                    "url": resp.content_uri,
+                }
+                await self._client.room_send(
+                    self._config.output[self.get_instance_name()].room_id,
+                    message_type="m.room.message",
+                    content=content
+                )
 
-    def _send_message(self, text):
-        self._loop.run_until_complete(self._send_message_impl(text))
+    def _send_message(self, text, files):
+        self._loop.run_until_complete(self._send_message_impl(text, files))
 
     def send_message(self, text: str, files: List[str] = []):
-        threading.Thread(target=self._send_message, args=(text,)).start()
+        threading.Thread(target=self._send_message, args=(text, files)).start()
         log.message(self.__class__.__name__, text, files)
 
     def get_name(self) -> str:
