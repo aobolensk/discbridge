e89e38c6d1e847a12abe042a74f2bf2888db8352
Arseniy Obolenskiy <gooddoog@student.su>
2022-08-24 15:29:29 +0300

    Handle 2k characters message limit on Discord output

--- output/discord.py
+++ output/discord.py
@@ -3,7 +3,7 @@ from typing import TYPE_CHECKING, List
 from config import Config
 from discord_webhook import DiscordWebhook
 from logger import log
-from utils import proxy
+from utils import proxy, split_by_chunks
 
 from output.abc import Handler
 
@@ -23,20 +23,23 @@ class DiscordHandler(Handler):
         }
 
     def send_message(self, text: str, files: List[str] = []) -> None:
-        webhook = DiscordWebhook(
-            url=self._config.output[self.get_instance_name()].webhook_link, rate_limit_retry=True,
-            content=text, proxies=self._proxy)
-        for file in files:
-            webhook.add_file(file=open(file, "rb").read(), filename=file)
-        resp = webhook.execute()
-        if resp.status_code in (200, 204):
-            log.message(self.get_instance_name(), text, files)
-        elif resp.status_code == 413:
-            text_ext = (
-                text + "\n" + f"`+ {len(files)} file(s) that couldn't be uploaded (too big to upload to Discord)`")
-            self._retry_without_files(text_ext)
-        else:
-            log.error(f"[{self.get_instance_name()}] ERROR: {resp} ({text})")
+        text_chunks = list(split_by_chunks(text, 2000))
+        for index, text_chunk in enumerate(text_chunks):
+            webhook = DiscordWebhook(
+                url=self._config.output[self.get_instance_name()].webhook_link, rate_limit_retry=True,
+                content=text_chunk, proxies=self._proxy)
+            if index + 1 == len(text_chunks):
+                for file in files:
+                    webhook.add_file(file=open(file, "rb").read(), filename=file)
+            resp = webhook.execute()
+            if resp.status_code in (200, 204):
+                log.message(self.get_instance_name(), text_chunk, files if index + 1 == len(text_chunks) else None)
+            elif resp.status_code == 413:
+                text_ext = (
+                    f"{text_chunk}\n`+ {len(files)} file(s) that couldn't be uploaded (too big to upload to Discord)`")
+                self._retry_without_files(text_ext)
+            else:
+                log.error(f"[{self.get_instance_name()}] ERROR: {resp} ({text_chunk})")
 
     def _retry_without_files(self, text: str) -> None:
         webhook = DiscordWebhook(
